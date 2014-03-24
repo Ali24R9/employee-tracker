458af9093c91bdb537d0bf3556e187eae27d7b1b
Drew Seminara & Ali Abou-Zahr <student@epicodus.com>
2014-03-24 12:31:52 -0700

    add and list employee, project, and division classes

---
+++ employee_tracker_ui.rb
@@ -0,0 +1,137 @@
+require 'active_record'
+require './lib/employee'
+require './lib/division'
+require './lib/project'
+
+datbase_configuration = YAML::load(File.open('./db/config.yml'))
+development_configuration = datbase_configuration['development']
+ActiveRecord::Base.establish_connection(development_configuration)
+
+def main_menu
+  puts "Welcome to the Employee Tracker!"
+  puts "Press 'e' for Employee Menu"
+  puts "Press 'd' for Division Menu"
+  puts "Press 'p' for Project Menu"
+  puts "Press 'x' to exit"
+
+  main_input = gets.chomp.downcase
+  case main_input
+  when 'e'
+    employee_menu
+  when 'd'
+    division_menu
+  when 'p'
+    project_menu
+  when 'x'
+    puts "Good-bye!"
+  else
+    puts "Please enter a valid option."
+    main_menu
+  end
+end
+
+def employee_menu
+  puts "Press 'a' to add a new employee"
+  puts "Press 'l' to list all the employees in the company database"
+  puts "Press 'm' to go back to the main menu"
+
+  employee_input = gets.chomp.downcase
+  case employee_input
+  when 'a'
+    employee_add
+  when 'l'
+    employee_list
+  when 'm'
+    main_menu
+  else
+    puts "Please enter a valid input"
+    employee_menu
+  end
+end
+
+def employee_add
+   puts "What employee would you like to add to the company database"
+   name = gets.chomp
+   employee = Employee.new({:name => name})
+   employee.save
+   puts "'#{name}' has been added to your database"
+   employee_menu
+end
+
+def employee_list
+  puts "Employee List: \n"
+  list = Employee.all
+  list.each { |employee| puts employee.name}
+  employee_menu
+end
+
+def division_menu
+  puts "Press 'a' to add a new division"
+  puts "Press 'l' to list all the divisions in the company database"
+  puts "Press 'm' to go back to the main menu"
+
+  input = gets.chomp
+  case input
+  when 'a'
+    division_add
+  when 'l'
+    division_list
+  when 'm'
+    main_menu
+  else
+    'please enter a valid input'
+    division_menu
+  end
+end
+
+def division_add
+  puts "Enter division name: "
+  name = gets.chomp
+  division = Division.new({:name => name})
+  division.save
+  puts "'#{name}' has been added to your database"
+  division_menu
+end
+
+def division_list
+  puts "Division List: "
+  list = Division.all
+  list.each { |division| puts division.name}
+  division_menu
+end
+
+def project_menu
+  puts "Press 'a' to add a new project"
+  puts "Press 'l' to list all the projects in the company database"
+
+  input = gets.chomp
+  case input
+  when 'a'
+    project_add
+  when 'l'
+    project_list
+  when 'm'
+    main_menu
+  else
+    'please enter a valid input'
+    project_menu
+  end
+end
+
+def project_add
+  puts "Enter project name: "
+  name = gets.chomp
+  project = Project.new({:name => name})
+  project.save
+  puts "'#{name}' has been added to your database"
+  project_menu
+end
+
+def project_list
+  puts "Project List: "
+  list = Project.all
+  list.each { |project| puts project.name}
+  project_menu
+end
+
+main_menu
